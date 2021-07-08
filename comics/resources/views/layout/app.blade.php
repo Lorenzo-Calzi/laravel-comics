826ac86e58b715e16eb0630883d86c730a77818d
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-08 15:54:01 +0200

    Aggiunto progetto comics

---
+++ comics/resources/views/layout/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        
+    </head>
+    <body>
+
+        
+        <header id="site_header">
+            <div class="up_header">
+                <div class="up_content">
+                    <span>DC POWER VISA</span>
+                    <span>ADDITIONAL DC SITES</span>
+                </div>
+            </div>
+
+            <div class="bottom_header">
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </div>
+        </header>
+        
+
+
+        <main id="site_main">
+            <div class="jumbotron">
+            </div>
+        
+        </main>
+    </body>
+</html>
